8ba36e0bd6e24d46435926fd5570a8fb155d2fa9
Axel Naumann <Axel.Naumann@cern.ch>
2014-10-15 18:08:10 +0200

    Parametrize output, input and directories.

--- build/unix/makeonepcm.sh
+++ build/unix/makeonepcm.sh
@@ -8,18 +8,25 @@
 
 srcdir=$1
 shift
+objdir=$1
+shift
+pchfile=$1
+shift
 modules=$1
 shift
+allHeaders=allHeaders.h
+allLinkdefs=allLinkDef.h
 
-
-rm -f include/allHeaders.h include/allHeaders.h.pch include/allLinkDef.h all.h cppflags.txt include/allLinkDef.h
+rm -f $allHeaders $allLinkdefs cppflags.txt
+# previous versions put these into include/
+rm -f $objdir/include/allHeaders.h $objdir/include/allLinkDef.h
 
 while ! [ "x$1" = "x" ]; do
-    echo '#include "'$1'"' >> all.h
+    echo '#include "'$1'"' >> $allHeaders
     shift
 done
 
-for dict in `find $modules -name 'G__*.cxx' 2> /dev/null | grep -v /G__Cling.cxx  | grep -v core/metautils/src/G__std_`; do
+for dict in `cd $objdir; find $modules -name 'G__*.cxx' 2> /dev/null | grep -v /G__Cling.cxx  | grep -v core/metautils/src/G__std_`; do
     dirname=`dirname $dict`                   # to get foo/src
     dirname=`echo $dirname | sed -e 's,/src$,,' -e 's,^[.]/,,' ` # to get foo/
 
@@ -38,14 +45,14 @@ for dict in `find $modules -name 'G__*.cxx' 2> /dev/null | grep -v /G__Cling.cxx
         selmodules="$selmodules$dirname "
     fi
 
-    awk 'BEGIN{START=-1} /includePaths\[\] = {/, /^0$/ { if (START==-1) START=NR; else if ($0 != "0") { sub(/",/,"",$0); sub(/^"/,"-I",$0); print $0 } }' $dict >> cppflags.txt
-    echo "// $dict" >> all.h
-#     awk 'BEGIN{START=-1} /payloadCode =/, /^;$/ { if (START==-1) START=NR; else if ($1 != ";") { code=substr($0,2); sub(/\\n"/,"",code); print code } }' $dict >> all.h
-    awk 'BEGIN{START=-1} /headers\[\] = {/, /^0$/ { if (START==-1) START=NR; else if ($0 != "0") { sub(/,/,"",$0); print "#include",$0 } }' $dict >> all.h
+    awk 'BEGIN{START=-1} /includePaths\[\] = {/, /^0$/ { if (START==-1) START=NR; else if ($0 != "0") { sub(/",/,"",$0); sub(/^"/,"-I",$0); print $0 } }' $objdir/$dict >> cppflags.txt
+    echo "// $dict" >> $allHeaders
+#     awk 'BEGIN{START=-1} /payloadCode =/, /^;$/ { if (START==-1) START=NR; else if ($1 != ";") { code=substr($0,2); sub(/\\n"/,"",code); print code } }' $dict >> $allHeaders
+    awk 'BEGIN{START=-1} /headers\[\] = {/, /^0$/ { if (START==-1) START=NR; else if ($0 != "0") { sub(/,/,"",$0); print "#include",$0 } }' $objdir/$dict >> $allHeaders
 
     if ! test "$dirname" = "`echo $dirname| sed 's,/qt,,'`"; then
         # something qt; undef emit afterwards
-        cat <<EOF >> all.h
+        cat <<EOF >> $allHeaders
 #ifdef emit
 # undef emit
 #endif
@@ -55,7 +62,7 @@ for dict in `find $modules -name 'G__*.cxx' 2> /dev/null | grep -v /G__Cling.cxx
 EOF
     elif ! test "$dirname" = "`echo $dirname| sed 's,net/ldap,,'`"; then
         # ldap; undef Debug afterwards
-        cat <<EOF >> all.h
+        cat <<EOF >> $allHeaders
 #ifdef Debug
 # undef Debug
 #endif
@@ -66,31 +73,28 @@ EOF
     fi
 
     find $srcdir/$dirname/inc/ -name '*LinkDef*.h' | \
-        sed -e 's|^|#include "|' -e 's|$|"|' >> alldefs.h
+        sed -e 's|^|#include "|' -e 's|$|"|' >> $allLinkdefs
 done
 
 echo
 echo Generating the one large pcm for $selmodules, patience...
 echo
 
-mv all.h include/allHeaders.h
-mv alldefs.h include/allLinkDef.h
-
 # check if rootcling_tmp exists in the expected location (not the case for CMake builds)
-if [ ! -x core/utils/src/rootcling_tmp ]; then
+if [ ! -x $objdir/core/utils/src/rootcling_tmp ]; then
   exit 0
 fi
 
-cxxflags="-D__CLING__ -D__STDC_LIMIT_MACROS -D__STDC_CONSTANT_MACROS -Iinclude -Ietc -Ietc/cling `cat cppflags.txt | sort | uniq`"
+cxxflags="-D__CLING__ -D__STDC_LIMIT_MACROS -D__STDC_CONSTANT_MACROS -I$objdir/include -I$objdir/etc -I$objdir/etc/cling `cat cppflags.txt | sort | uniq`"
 rm cppflags.txt
 
 # generate one large pcm
 rm -f allDict.* lib/allDict_rdict.pc*
-touch etc/allDict.cxx.h
-core/utils/src/rootcling_tmp -1 -f etc/allDict.cxx -noDictSelection -c $cxxflags -I$srcdir include/allHeaders.h include/allLinkDef.h
+touch allDict.cxx.h
+$objdir/core/utils/src/rootcling_tmp -1 -f allDict.cxx -noDictSelection -c $cxxflags $allHeaders $allLinkdefs
 res=$?
 if [ $res -eq 0 ] ; then
-  mv etc/allDict_rdict.pch etc/allDict.cxx.pch
+  mv allDict_rdict.pch $pchfile
   res=$?
 
   # actually we won't need the allDict.[h,cxx] files
