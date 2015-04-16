5d451fdfc4c413e3637b27cb7ec672991b9fdc3f
Axel Naumann <Axel.Naumann@cern.ch>
2015-04-16 14:32:43 +0200

    Move string_view detection into configure.

--- core/metautils/inc/RStringView.h
+++ core/metautils/inc/RStringView.h
@@ -12,42 +12,16 @@
 #ifndef RStringView_H
 #define RStringView_H
 
-#if defined(__has_include)
+#include "RConfigure.h"
 
-// If the compiler is g++ and we are not in C++14 or higher and the standard
-// library is not LLVM's libcxx, there is no point in checking for
-// string_view.
-#if ((!defined(__clang__)&&defined(__GNUC__)) || defined(__CLING__GNUC__)) &&  __cplusplus <= 201103L \
-    && !defined(_LIBCPP_VERSION)
-// Don't check with GCC and less than C++14
-#else
-#define R__CHECK_FOR_STRING_VIEW
-#endif
-
-#endif
-
-#ifdef R__CHECK_FOR_STRING_VIEW
-#if __has_include("string_view")
-
-#define R_HAS_std_string_view_header
-
-#elif __has_include("experimental/string_view")
-
-#define R_HAS_lib_std_experimental_string_view
-
-#endif
-#endif // R__CHECK_FOR_STRING_VIEW
-
-#ifdef R_HAS_std_string_view_header
+#ifdef R__HAS_STD_STRING_VIEW
 #include <string_view>
-#elif defined(R_HAS_lib_std_experimental_string_view)
-#define R_HAS_std_experimental_string_view
-#include <experimental/string_view>
 #else
-#include "RWrap_libcpp_string_view.h"
-#endif
-
-#if defined( R_HAS_std_experimental_string_view )
+# if defined(R__HAS_STD_EXPERIMENTAL_STRING_VIEW)
+#  include <experimental/string_view>
+# else
+#  include "RWrap_libcpp_string_view.h"
+# endif
 
 namespace std {
 
@@ -68,6 +42,6 @@ namespace std {
 //   }
 }
 
-#endif // R_HAS_std_experimental_string_view
+#endif // ifdef else R__HAS_STD_STRING_VIEW
 
 #endif // RStringView_H
